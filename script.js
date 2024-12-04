dacd16d2695c02979aced79002107333056f7504
it904 <khanabdulwassay574@gmail.com>
2024-12-04 22:52:11 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// JavaScript to trigger animation on load
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        alert('Happy Birthday! 🎉 Hope you have a wonderful day!');
+    }, 2000);
+});
